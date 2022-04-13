3429264a029da89809a4cfe5edc37e8bbde45c4e
TORCHIZM <ismailpvo1535@gmail.com>
2022-04-13 17:00:02 +0300

    hotfix

--- routers/user.go
+++ routers/user.go
@@ -2,7 +2,6 @@ package routers
 
 import (
 	"torchizm/library-backend/api/auth"
-	"torchizm/library-backend/api/user"
 	"torchizm/library-backend/middlewares"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,7 +10,6 @@ import (
 // SetupRoutes Employe
 func UserRoutes(ctx fiber.Router) {
 	route := ctx.Group("/user")
-	route.Get("/", user.GetAll)
 	route.Post("/login", auth.Login)
 	route.Post("/register", auth.Register)
 	route.Post("/resend-confirmation", auth.ResendMail)
